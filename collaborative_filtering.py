Fixed Normalisation, Removed Database links from Individual Algorithms using MovieLens class instead, Normalised Content Based Filtering
Normalised Content Based Filtering works when there is no need to calculate similarity metrics

collaborative_filtering.py
@@ -1,162 +1,49 @@
-import numpy as np
 import core
-from dataset import *
 
 cf_table={'item': 'cf_item_recommendation',
           'user': 'cf_n_similar_users',
           'user_recommendation': 'cf_user_recommendation'
           }
-class PrepareCollaborativeFiltering:
 
-    def __init__(self,db):
-        self.db = db
-        self.ratings = Movielens(db).load_ratings()
-        self.limit = 20
+class CollaborativeFiltering:
 
-        self.generate_user_similarity_matrix()
-        self.generate_item_similarity_matrix()
+    def fit(self, ratings):
+        # user similarity matrix
+        self.user_similarities = core.pairwise_cosine(ratings)
 
-        self.save_complete_item_similarities_to_database()
-        self.save_complete_similar_users_to_database()
-        self.save_recommendation_for_user()
-
-    # Item Similarity
-    def generate_item_similarity_matrix(self):
-        data = self.ratings.transpose()
+        # item similarity matrix
+        data = ratings.transpose()
         data[data > 0] = 1
         self.item_similarities = core.pairwise_cosine(data)
 
-    def save_complete_item_similarities_to_database(self):
-        similar_movies = []
+        self.ratings = ratings
 
-        for i in range(0, self.ratings.shape[1]):
-            predicted = self.predict_by_item_similarirty(i)
-            similar_movies.append(predicted)
-        df = pd.DataFrame(similar_movies)
-        df['id']=df.index
-        self.db.save_entire_df(df, cf_table['item'])
+    def predict(self, user_id= None, item_id = None, limit = 10):
+        if user_id is None and item_id is None:
+            raise('No parameter provided for user_id or item_id')
+        if user_id:
+            return self.predict_for_user(user_id, limit = limit)
+        else:
+            return self.predict_for_item(item_id, limit = limit)
 
-    def predict_by_item_similarirty(self, item_id):
-        arguments_sorted =  core.reverse_argsort(self.item_similarities[item_id])
-        arguments_sorted = arguments_sorted[arguments_sorted != item_id][:self.limit]
+    def predict_for_item(self, item_id, limit=10):
+        arguments_sorted = core.reverse_argsort(self.item_similarities[item_id])
+        arguments_sorted = arguments_sorted[arguments_sorted != item_id][:limit+1]
         return arguments_sorted
 
-    # User Similarity
-
-    def generate_user_similarity_matrix(self):
-        self.user_similarities = core.pairwise_cosine(self.ratings)
-
-    def top_n_similar_users(self,user_id):
-        return np.argsort(self.user_similarities[user_id])[::-1][1:11]
-
-    def user_similarity_for_index(self,row, column):
-        return self.user_similarities[row, column]
-
-    def save_complete_similar_users_to_database(self):
-        values = []
-
-        for i in range(0, self.ratings.shape[0]):
-            predicted = self.top_n_similar_users(i)
-            similarity = self.user_similarity_for_index(i,predicted)
-            user_id = np.array([i for j in range(predicted.shape[0])])
-            data = pd.DataFrame([user_id,predicted,similarity])
-            data = data.transpose()
-            values.append(data)
-
-        df = pd.concat(values)
-        renaming = {0: 'id', 1: 'similar_user', 2: 'similarity'}
-        df = df.rename(columns = renaming)
-
-        df['id'] = df['id'].astype(int)
-        df['similar_user'] = df['similar_user'].astype(int)
-        #print(df)
-        self.db.save_entire_df(df, cf_table['user'])
-
-    def predict_by_user_similarity(self, user_id):
-        top_10_similar_users = np.argsort(self.user_similarities[user_id])[::-1][1:11]
+    def predict_for_user(self, user_id, limit=10):
+        top_10_similar_users = core.reverse_argsort(self.user_similarities[user_id])[1:limit+1]
 
         value = []
 
         for i in range(self.ratings.shape[0]):
-            if self.ratings[user_id,i] != 0:
+            if self.ratings[user_id, i] != 0:
                 value.append(0)
                 continue
             weightage_of_item = 0
             for k in top_10_similar_users:
-                weightage_of_item+=self.ratings[k,i]*self.user_similarities[user_id,k]
+                weightage_of_item += self.ratings[k, i] * self.user_similarities[user_id, k]
 
             value.append(weightage_of_item)
 
-        return np.argsort(value)[:self.limit]
-
-    def save_recommendation_for_user(self):
-        recommendation = []
-        print(self.user_similarities.shape[0])
-        for i in range(self.user_similarities.shape[0]):
-            print(i)
-            prediction = self.predict_by_user_similarity(i)
-            recommendation.append(prediction)
-        df = pd.DataFrame(recommendation)
-        df['id'] = df.index
-        print(df)
-        self.db.save_entire_df(df, table_name=cf_table['user_recommendation'])
-
-class CollaborativeFiltering:
-
-    def limit_number_of_recommendations(self,limit):
-        self.limit = limit
-
-
-    def reload_conditions(self):
-        return not self.db.table_exists(cf_table['user'],cf_table['item'],cf_table['user_recommendation'])
-
-    def __init__(self, db, clear_cache=False):
-        self.db = db
-        ml = Movielens(db)
-        self.user_similarities_table = 'collaborative_user_similarities'
-
-        self.user_recommendation_table = 'collaborative_user_recommendation';
-
-        if self.reload_conditions() or clear_cache:
-            PrepareCollaborativeFiltering(db)
-
-    def predict_for_item(self, item_id):
-        prediction = self.db.get(cf_table['item'], where = 'id = '+str(item_id))
-        prediction = prediction.drop(['index', 'id'], axis=1)
-        return prediction.values[0]
-
-    def generate_prediction(self, user_id):
-        top_10_similar_users = self.db.get(cf_table['user'], where = 'id = '+ str(user_id))
-
-        top_10_similar_users = top_10_similar_users.drop(['index', 'id'], axis=1)
-
-        similarities = dict(zip(top_10_similar_users['similar_user'].tolist(), top_10_similar_users['similarity'].tolist()))
-        # get ratings
-        similar_users_ratings = self.db.get('ratings', where = 'userId in ('+','.join(map(str,similarities))+')')
-        similar_users_ratings = similar_users_ratings.drop(['index', 'timestamp'], axis=1)
-
-        user_ratings = self.db.get('ratings', where = 'userId = '+str(user_id))
-
-        # remove things which are already rated by the user
-
-        similar_users_ratings = similar_users_ratings[~similar_users_ratings['movieId'].isin(user_ratings['movieId'])]
-
-        movies = similar_users_ratings['movieId'].unique().tolist()
-
-        movie_weightage = []
-
-        for i in movies:
-            user_who_have_rated = similar_users_ratings[similar_users_ratings['movieId']==i]
-
-            user_who_have_rated['weightage'] = user_who_have_rated['rating'] * user_who_have_rated['userId'].map(similarities)
-            movie_weightage.append([i, user_who_have_rated['weightage'].sum()])
-
-        df = pd.DataFrame(movie_weightage, columns=['id','weightage'])
-        df = df.sort_values('weightage', ascending=False)
-        return df.head(10)['id'].tolist()
-
-    def predict_for_user(self, user_id):
-        recommendation = self.db.get(cf_table['user_recommendation'], where = 'id = '+str(user_id))
-        recommendation = recommendation.drop(['index','id'], axis =1)
-        return recommendation.values[0]
-        # return self.generate_prediction(user_id)
+        return core.reverse_argsort(value)[:limit+1]

content_based_filtering.py
@@ -1,100 +1,52 @@
-import numpy as np
 import core
 from dataset import *
 import pandas as pd
-class ContentBasedFiltering:
+import numpy as np
+class Simple_ContentBasedFiltering:
 
-    def limit_number_of_recommendations(self,limit):
-        self.limit = limit
+    def fit(self, matrix):
+        self.item_similarities = core.pairwise_cosine(matrix)
 
-    def __init__(self,db, regenerate = False):
-        self.db = db
-        self.table = 'content_based_recommendations'
-
-        if regenerate or not db.table_exists(self.table):
-            self.generate_similarity_matrix()
-            self.limit_number_of_recommendations(20)
-            self.store_top_n_similarities_to_database()
-
-    # fetch prediction from database
     def predict(self, item_id):
-        prediction = self.db.get(self.table, where='id = ' + str(item_id))
-        prediction = prediction.drop(['index', 'id'], axis=1)
-        return prediction.values[0]
-
-    # regeneration of similarity matrix
-    def generate_similarity_matrix(self):
-        ml = Movielens(self.db)
-        self.item_similarities = core.pairwise_cosine(ml.load_complete_movie_info())
-
-    def predict_using_similarirty_matrix(self, item_id):
         arguments_sorted = core.reverse_argsort(self.item_similarities[item_id])
 
         # select everything except item_id; else same movie will be recommended
         arguments_sorted = arguments_sorted[arguments_sorted!=item_id]
 
-        #limit to number of output
-        arguments_sorted = arguments_sorted[: self.limit+1]
-
         return arguments_sorted
-
-    def store_top_n_similarities_to_database(self):
-        similar_movies = []
-        for i in range(0, self.item_similarities.shape[0]):
-            predicted = self.predict_using_similarirty_matrix(i)
-            similar_movies.append(predicted)
-        df = pd.DataFrame(similar_movies)
-        df['id']=df.index
-        self.db.save_entire_df(df, self.table)
 
 class Normalised_ContentBasedFiltering:
 
-    def limit_number_of_recommendations(self,limit):
-        self.limit = limit
+    similarity_flag = False
 
-    def __init__(self,db, regenerate = False):
-        self.db = db
-        self.table = 'normalised_content_based_recommendations'
+    def fit(self, matrix):
+        self.normalise_mapper, self.normalised_data = core.normalise_dataframe(matrix)
 
-        self.ml = Movielens(self.db)
-        self.normalise()
-        exit()
-        if regenerate or not db.table_exists(self.table):
-            self.generate_similarity_matrix()
-            self.limit_number_of_recommendations(20)
-            self.store_top_n_similarities_to_database()
+    def get_movies_of_type(self, movie_type):
+        return self.normalise_mapper[self.normalise_mapper['normalised_key'] == movie_type].index.values
 
-    def normalise(self):
-        a = core.normalise_dataframe(self.ml.load_complete_movie_info())
-        print(a)
+    def similar_movies_of_type(self, movie_type):
+        similar_movies = core.reverse_argsort(self.normalised_similarity[movie_type])
+        return similar_movies
 
-    # fetch prediction from database
-    def predict(self, item_id):
-        prediction = self.db.get(self.table, where='id = ' + str(item_id))
-        prediction = prediction.drop(['index', 'id'], axis=1)
-        return prediction.values[0]
+    def predict(self, item_id, limit = 10):
+        movie_type = self.normalise_mapper['normalised_key'].iloc[item_id]
 
-    # regeneration of similarity matrix
-    def generate_similarity_matrix(self):
-        ml = Movielens(self.db)
-        self.item_similarities = core.pairwise_cosine(ml.load_complete_movie_info())
+        # simple lookup
+        selected_movies = self.get_movies_of_type(movie_type)
+        selected_movies = selected_movies[np.where(selected_movies != item_id)]
+        if selected_movies.shape[0]>limit:
+             return selected_movies
 
-    def predict_using_similarirty_matrix(self, item_id):
-        arguments_sorted = core.reverse_argsort(self.item_similarities[item_id])
+        # If don't have the required number of movies of same type, find similar movie_type
+        if not self.similarity_flag:
+            # since upper part was a simple lookup finding similarity matrix is not compulsary
+            self.normalised_similarity = core.pairwise_cosine(self.normalised_data)
+            self.similarity_flag = True
 
-        # select everything except item_id; else same movie will be recommended
-        arguments_sorted = arguments_sorted[arguments_sorted!=item_id]
+        similar_movies = self.similar_movies_of_type(movie_type)
 
-        #limit to number of output
-        arguments_sorted = arguments_sorted[: self.limit+1]
-
-        return arguments_sorted
-
-    def store_top_n_similarities_to_database(self):
-        similar_movies = []
-        for i in range(0, self.item_similarities.shape[0]):
-            predicted = self.predict_using_similarirty_matrix(i)
-            similar_movies.append(predicted)
-        df = pd.DataFrame(similar_movies)
-        df['id']=df.index
-        self.db.save_entire_df(df, self.table)
+        for i in similar_movies.tolist()[1:]:
+            selected_movies = np.append(selected_movies,self.get_movies_of_type(movie_type))
+            if selected_movies.shape[0] > limit:
+                return selected_movies

core.py
@@ -9,6 +9,8 @@ import pytz
 import json
 import os
 import pandas as pd
+import random
+
 # Logging
 
 LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -38,6 +40,11 @@ def pairwise_cosine(data):
 def file_exists(location):
     path = pathlib.Path(location)
     return path.exists()
+
+def randomly_select_items(array, limit=None):
+    if limit:
+        return random.choice(array)[:limit]
+    return random.choice(array)
 
 def union(lists):
     return reduce(np.union1d, lists)
@@ -75,8 +82,6 @@ def current_seconds():
 # pass in a dataframe, it will return key (normalised keys for original df, normalised dataframe)
 # only pass the dataframe with specific columns that needs normalisation
 def normalise_dataframe(df):
-    count = 0
-
     original_index = df.index.values
 
     new_index = []
@@ -93,8 +98,8 @@ def normalise_dataframe(df):
             new_index.append(position)
         except:
             normalised_string_list.append(string_version)
-            count = count + 1
             new_index.append(count)
+            count = count + 1
 
     df['normalised_key'] = new_index
 

recommender.py
@@ -8,6 +8,8 @@ from dataset import *
 from hybridization import Hybridization
 from json_formater import JSON_formatter
 
+from time import time
+
 if not core.file_exists('defaults.json'):
     generate_defaults.generate_defaults()
 
@@ -21,28 +23,59 @@ if not db.table_exists('ratings', 'movies', 'links', 'movies_mapped'):
 ml = Movielens(db)
 
 # recommendation on
-movies_id = 200
+movies_id = 7
 
 movie_title = ml.get_movie_names([movies_id]).loc[0,'title']
 user_id = 3
 
-cb = ContentBasedFiltering(db)
+print(ml.get_movie_type(movies_id))
 
-print('Content Based Recommendation for', movie_title)
-cb_recommendation = cb.predict(movies_id)
-print(cb_recommendation)
+start = time()
+nb = Normalised_ContentBasedFiltering()
+nb.fit(ml.load_complete_movie_info())
+fitting = time() - start
 
-ncb = Normalised_ContentBasedFiltering(db)
-exit()
+print('Fit time = ',fitting)
+ncb_recommendation = nb.predict(movies_id)
+
+# for i in range(ml.load_complete_movie_info().shape[0]):
+#     ncb_recommendation = nb.predict(i)
+
+end =  time()
+total_ncb = end - start
+
+# print(ncb_recommendation)
+
+start = time()
+cb = ContentBasedFiltering()
+cb.fit(ml.load_complete_movie_info())
+fitting = time() - start
+
+print('Fit time = ',fitting)
+cb_recommendation = cb.predict(movies_id)[:ncb_recommendation.shape[0]]
+
+# for i in range(ml.load_complete_movie_info().shape[0]):
+#     cb_recommendation = cb.predict(i)[:ncb_recommendation.shape[0]]
+end =  time()
+total_cb = end - start
+
+
+#print(cb_recommendation)
+
+print('number of items in simple cb ', cb_recommendation.shape,  'time taken',total_cb)
+print('number of items in normalised cb ', ncb_recommendation.shape, 'time taken',total_ncb)
+print('number of same items ', np.intersect1d(cb_recommendation,ncb_recommendation).shape)
+
 # Collaborative Filtering
 
-cf = CollaborativeFiltering(db, clear_cache=False)
+cf = Simple_ContentBasedFiltering()
+cf.fit(ml.load_ratings())
 
 print('Collaborative Filtering Recommendation for item:',movie_title)
-cf_item_recommendation = cf.predict_for_item(movies_id)
+cf_item_recommendation = cf.predict(item_id=movies_id)
 print(cf_item_recommendation)
 
-cf_user_recommendation = cf.predict_for_user(user_id)
+cf_user_recommendation = cf.predict_for_user(user_id = user_id)
 print('Collaborative Filtering Recommendation for user:', user_id)
 print(cf_user_recommendation)
 